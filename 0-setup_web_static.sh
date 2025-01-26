8a5b2530730a4f6aaf3f0dce67efcb97b7754e48
nhonoree <honoreniyirema@gmail.com>
2025-01-26 09:51:26 -0800

    updates

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@
 sudo apt-get update
 sudo apt-get install -y nginx
 
-# Create the necessary directories with correct permissions
+# Create the necessary directories if they don't already exist
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
@@ -18,10 +18,11 @@ echo "<html>
   </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Set permissions to the directories and files
+# Set ownership of /data/ to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Create a symbolic link /data/web_static/current linked to the test folder
+# Create a symbolic link /data/web_static/current linked to /data/web_static/releases/test/
+# If the symbolic link exists, it will be deleted and recreated
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/
